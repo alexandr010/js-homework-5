c77245f7d5edc8227b30aa26f3b6d9458df11762
Alexandr Somov <aleksandr.somov2011@gmail.com>
2020-09-06 12:52:05 +0300

    step-1-modify

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 function createCarousel(slidesCount) {
-  let carousel = document.createElement('div');
+  const carousel = document.createElement('div');
   carousel.setAttribute('id', 'carousel');
   carousel.className = 'carousel';
   document.body.prepend(carousel);
 
-  let slideList = document.createElement('ul');
+  const slideList = document.createElement('ul');
   slideList.className = 'slides';
   carousel.append(slideList);
 
   for (let i = 0; i < slidesCount; i++) {
-    let slidesItem = document.createElement('li');
+    const slidesItem = document.createElement('li');
     slidesItem.className = 'slides__item';
-  slideList.appendChild(slidesItem);
+    slideList.appendChild(slidesItem);
     i === 0 && slidesItem.classList.add('active');
     slidesItem.innerHTML = `<a href = #>${i}</a>`;
   }
 
-  let indicator = document.createElement('div');
+  const indicator = document.createElement('div');
   indicator.className = 'indicators';
   carousel.append(indicator);
 
   for (let i = 0; i < slidesCount; i++) {
-    let indicatorItem = document.createElement('span');
+    const indicatorItem = document.createElement('span');
     indicatorItem.className = 'indicators__item';
     i === 0 && indicatorItem.classList.add('active');
     indicatorItem.setAttribute('data-slide-to', `${i}`);
     indicator.append(indicatorItem);
   }
-  let control = document.createElement('div');
+  const control = document.createElement('div');
   control.className = 'controls';
   carousel.append(control);
   const NEXT = `<div class = "controls__item controls__next"><i class = "fas fa-chevron-right"></i></div>`;
@@ -35,16 +35,15 @@ function createCarousel(slidesCount) {
   const PAUSE = `<div class = "controls__item controls__pause"><i class = "fas fa-play"></i></div>`;
   control.innerHTML = PREV + NEXT + PAUSE;
 
-  let styleTag = document.createElement('style');
+  const styleTag = document.createElement('style');
   carousel.append(styleTag);
   // styleTag.setAttribute('style', 'display: block');
 
-  let container = document.querySelector('#carousel');
-  let slide = container.querySelector('.slides');
-  let slideItem = slide.querySelectorAll('.slides__item');
+  const container = document.querySelector('#carousel');
+  const slide = container.querySelector('.slides');
+  const slideItem = slide.querySelectorAll('.slides__item');
 
-  styleTag.innerHTML = `.slides{display: flex; background-color: red; list-style: none}
-    `;
+  styleTag.innerHTML = `.slides{display: flex; background-color: red; list-style: none}`;
 
   console.log(slideItem);
  }
