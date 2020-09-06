e3d2bbb9d5087642fd5f5745f2dbda3fc3a1b2e1
Alexandr Somov <aleksandr.somov2011@gmail.com>
2020-09-06 12:48:27 +0300

    step-1

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function createCarousel(slidesCount) {
   let carousel = document.createElement('div');
+  carousel.setAttribute('id', 'carousel');
   carousel.className = 'carousel';
   document.body.prepend(carousel);
 
@@ -10,9 +11,9 @@ function createCarousel(slidesCount) {
   for (let i = 0; i < slidesCount; i++) {
     let slidesItem = document.createElement('li');
     slidesItem.className = 'slides__item';
-    slideList.appendChild(slidesItem);
+  slideList.appendChild(slidesItem);
     i === 0 && slidesItem.classList.add('active');
-    slidesItem.innerHTML = `<a href = #></a>`;
+    slidesItem.innerHTML = `<a href = #>${i}</a>`;
   }
 
   let indicator = document.createElement('div');
@@ -26,20 +27,25 @@ function createCarousel(slidesCount) {
     indicatorItem.setAttribute('data-slide-to', `${i}`);
     indicator.append(indicatorItem);
   }
+  let control = document.createElement('div');
+  control.className = 'controls';
+  carousel.append(control);
+  const NEXT = `<div class = "controls__item controls__next"><i class = "fas fa-chevron-right"></i></div>`;
+  const PREV = `<div class = "controls__item controls__prev"><i class = "fas fa-chevron-left"></i></div>`;
+  const PAUSE = `<div class = "controls__item controls__pause"><i class = "fas fa-play"></i></div>`;
+  control.innerHTML = PREV + NEXT + PAUSE;
 
+  let styleTag = document.createElement('style');
+  carousel.append(styleTag);
+  // styleTag.setAttribute('style', 'display: block');
+
+  let container = document.querySelector('#carousel');
+  let slide = container.querySelector('.slides');
+  let slideItem = slide.querySelectorAll('.slides__item');
+
+  styleTag.innerHTML = `.slides{display: flex; background-color: red; list-style: none}
+    `;
+
+  console.log(slideItem);
  }
 createCarousel(5);
-
-// function getListContent() {
-// let fragment = new DocumentFragment();
-
-// for(let i=1; i<=3; i++) {
-//     let li = document.createElement('li');
-//     li.append();
-//     fragment.append(li);
-//   }
-
-//   return fragment;
-// }
-
-// slideList.append(getListContent());
